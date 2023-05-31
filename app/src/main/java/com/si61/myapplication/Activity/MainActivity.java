bbdd04e9f576d47c2b27020f6d0b3b6d0d3317e9
ChristianErvinaWijaya <christianervinawijaya@mhs.mdp.ac.id>
2023-05-31 17:34:23 +0700

    Lanjutan video part 1

--- app/src/main/java/com/si61/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/si61/myapplication/Activity/MainActivity.java
@@ -8,8 +8,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.si61.myapplication.API.APIRequestData;
+import com.si61.myapplication.API.RetroServer;
+import com.si61.myapplication.Adapter.AdapterWisata;
 import com.si61.myapplication.Model.ModelResponse;
 import com.si61.myapplication.Model.ModelWisata;
 import com.si61.myapplication.R;
@@ -17,10 +21,15 @@ import com.si61.myapplication.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvWisata;
     private ProgressBar pbWisata;
     private FloatingActionButton fabTambah;
+
     private RecyclerView.Adapter adWisata;
     private RecyclerView.LayoutManager lmWisata;
     private List<ModelWisata> listwisata = new ArrayList<>();
@@ -47,7 +56,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void retrieveWisata(){
+        pbWisata.setVisibility(View.VISIBLE);
 
+        APIRequestData API = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ModelResponse> proses = API.ardRetrieve();
+
+        proses.enqueue(new Callback<ModelResponse>() {
+            @Override
+            public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                String kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+                listwisata = response.body().getData();
+
+                adWisata = new AdapterWisata(MainActivity.this, listwisata);
+                rvWisata.setAdapter(adWisata);
+                adWisata.notifyDataSetChanged();
+
+                pbWisata.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error: Gagal Menghubungi Server!", Toast.LENGTH_SHORT).show();
+                pbWisata.setVisibility(View.GONE);
+
+            }
+        });
     }
 
     @Override
